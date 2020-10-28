471d5832c7c07e36c3ab898ebddff9fc6addc87b
Dipta Das <dipta670@gmail.com>
2020-10-27 20:03:45 -0500

    Disable IEEE API test

--- src/test/java/edu/baylor/ecs/ams/IEEETests.java
+++ src/test/java/edu/baylor/ecs/ams/IEEETests.java
@@ -3,6 +3,7 @@ package edu.baylor.ecs.ams;
 import edu.baylor.ecs.ams.model.BaseModel;
 import edu.baylor.ecs.ams.runner.IEEERunner;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -13,6 +14,7 @@ import java.util.List;
 @Slf4j
 class IEEETests {
 
+    @Disabled
     @Test
     void ieeeSearchQuery() throws IOException, InterruptedException {
         String query = "((\"Abstract\":code smells) AND \"Full Text Only\":microservice)";
