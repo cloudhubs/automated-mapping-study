5ed4e072e7fb0339dfd116a43ae2d0d0de8116e2
Dipta Das <dipta670@gmail.com>
2020-10-28 01:57:39 -0500

    Fixed antlr4 error handling

--- src/main/java/edu/baylor/ecs/ams/query/QueryParser.java
+++ src/main/java/edu/baylor/ecs/ams/query/QueryParser.java
@@ -1,26 +1,36 @@
 package edu.baylor.ecs.ams.query;
 
+import lombok.extern.slf4j.Slf4j;
 import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.misc.ParseCancellationException;
 import org.antlr.v4.runtime.tree.ParseTree;
 
+@Slf4j
 public class QueryParser {
-    public static ParseTree parse(String input) throws Exception {
+
+    public static ParseTree parse(String input) {
         CodePointCharStream source = CharStreams.fromString(input);
         BooleanQueryLexer lexer = new BooleanQueryLexer(source);
         CommonTokenStream tokenStream = new CommonTokenStream(lexer);
         BooleanQueryParser parser = new BooleanQueryParser(tokenStream);
 
-        parser.addErrorListener(new BaseErrorListener() {
-            @Override
-            public void syntaxError(Recognizer<?, ?> recognizer, Object o, int i, int i1, String s, RecognitionException e) {
-                throw e;
-            }
-        });
+        lexer.addErrorListener(new ThrowingErrorListener());
+        parser.addErrorListener(new ThrowingErrorListener());
 
         try {
             return parser.query();
-        } catch (Exception e) {
-            throw new Exception(e);
+        } catch (ParseCancellationException e) {
+            log.error(e.toString());
+            throw e;
         }
     }
+
+    public static class ThrowingErrorListener extends BaseErrorListener {
+        @Override
+        public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e)
+                throws ParseCancellationException {
+            throw new ParseCancellationException("line " + line + ":" + charPositionInLine + " " + msg);
+        }
+    }
+
 }
